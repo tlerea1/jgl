30dc3f5308bf3c4c86fe70985bfa853e66b223c5
Tuvia Lerea <tuvia.lerea@gmail.com>
2014-06-18 12:43:49 -0400

    added stencil functionality. changed flush to work with stencilling, seems to work

--- jgllib.js
+++ jgllib.js
@@ -13,8 +13,11 @@
 
 //--------------------------Setup and Globals----------------------
 var screen;
+var mouse;
 var backCtx;
 var backCanvas;
+var stencilCanvas;
+var stencilCtx;
 
 function Screen() {	
 	var c = document.getElementById("canvas");
@@ -23,17 +26,50 @@ function Screen() {
 	this.context = c.getContext("2d");
 	this.height = $("#canvas").height();
 	this.width = $("#canvas").width();
-	this.screenWidth = (window.screen.width / window.devicePixelRatio) * (1/ 96.0);
-	this.screenHeight = (window.screen.height / window.devicePixelRatio) * (1/ 96.0);
-//	this.viewDistance = div.getAttribute("width");
+	this.stencils = [];
+	this.useStencil = false;
+	this.stencilSelected = 0;
+//	this.screenWidth = (window.screen.width / window.devicePixelRatio) * (1/ 96.0);
+//	this.screenHeight = (window.screen.height / window.devicePixelRatio) * (1/ 96.0);
+//	this.viewDistance = java.awt.Toolkit.getDefaultToolkit().getScreenResolution();
+//	this.viewDistance = document.size.ppi();
+}
+
+function Mouse() {
+	this.buttons = [];
+	this.x = 0;
+	this.y = 0;
 }
 
 $(document).ready( function() {
 	screen = new Screen();
+	mouse = new Mouse();
 	backCanvas = document.createElement('canvas');
 	backCanvas.width = screen.width;
 	backCanvas.height = screen.height;
 	backCtx = backCanvas.getContext("2d");
+	stencilCanvas = document.createElement('canvas');
+	stencilCanvas.width = screen.width;
+	stencilCanvas.height = screen.height;
+	stencilCtx = stencilCanvas.getContext("2d");
+	$("body").append("<div id=\"inchBox\" style=\"width: 1in\"></div>");
+	screen.viewDistance = $("#inchBox").width();
+	
+	// The following three events track the mouse
+	// while it is inside the window.
+	// The mouse.buttons array is a logical array, 1 means pressed
+	$(window).mousemove(function(event){
+		mouse.x = event.pageX;
+		mouse.y = event.pageY;
+	});
+	$(window).mousedown(function(event){
+		var button = event.which;
+		mouse.buttons[button - 1] = 1;
+	});
+	$(window).mouseup(function(event) {
+		var button = event.which;
+		mouse.buttons[button - 1] = 0;
+	});
 });
 
 //----------------------Main Screen Functions--------------------
@@ -46,13 +82,31 @@ function jglClose() {
 	
 }
 
+/**
+ * Draws the off-screen canvas to the on-screen one. 
+ * Operates in discrete frames, meaning that each draw
+ * draws all the elements to the screen at once. If a stencil
+ * is selected works with the backCanvas and stencilCanvas to maintain
+ * discrete frames, read Stencil Comment for more information.
+ */
 function jglFlush() {
 	//screen.context.restore();
 
 //	screen.context.stroke();
-	screen.context.clearRect(0,0,screen.width,screen.height);
-	screen.context.drawImage(backCanvas, 0, 0);
-	backCtx.clearRect(0,0, backCanvas.width, backCanvas.height);
+	if (! screen.useStencil) {
+		screen.context.clearRect(0,0,screen.width,screen.height);
+		screen.context.drawImage(backCanvas, 0, 0);
+		backCtx.clearRect(0,0, backCanvas.width, backCanvas.height);
+	} else {
+		screen.context.clearRect(0,0,screen.width,screen.height);
+		stencilCtx.drawImage(screen.stencils[screen.stencilSelected], 0, 0);
+		stencilCtx.save();
+		stencilCtx.globalCompositeOperation = "source-in";
+		stencilCtx.drawImage(backCanvas, 0, 0);
+		stencilCtx.restore();
+		screen.context.drawImage(stencilCanvas, 0, 0);
+		stencilCtx.clearRect(0,0, stencilCanvas.width, stencilCanvas.height);
+	}
 
 }
 
@@ -180,6 +234,7 @@ function jglPolygon(x, y, color) {
 		throw "Polygon arrays not same length";
 	}
 	backCtx.fillStyle = color;
+	backCtx.strokeStyle = color;
 	backCtx.beginPath();
 	backCtx.moveTo(x[0], y[0]);
 	for (var i=1;i<x.length;i++) {
@@ -187,6 +242,7 @@ function jglPolygon(x, y, color) {
 	}
 	backCtx.closePath();
 	backCtx.fill();
+//	backCtx.stroke();
 }
 
 
@@ -238,7 +294,102 @@ function jglTextSet(fontName, fontSize, fontColor, fontBold, fontItalic) {
 	backCtx.fillStyle = fontColor;
 }
 
+/**
+ * Draws the given text starting at (x, y)
+ * @param text the text to be drawn
+ * @param x the x coordinate of the beginning of the text
+ * @param y the y coordinate of the beginning of the text
+ */
 function jglTextDraw(text, x, y) {
 	backCtx.fillText(text, x, y);
 }
+
+
+//------------------------Keyboard and Mouse functions ---------------------
+
+/**
+ * A function for getting information about the mouse.
+ * @return A Mouse object, contains x, y, and buttons 
+ * fields. buttons is a logical array, 1 means that button
+ * is pressed.
+ */
+function jglGetMouse() {
+	return mouse;
+}
+
+
+
+//-----------------------Stencil Functions----------------------------
+/*
+ * A Note on how stencils work:
+ * Stencils must be used in the following flow:
+ * createBegin(i)
+ * Drawing functions...
+ * createEnd
+ * select(i)
+ * drawing functions...
+ * flush
+ * 
+ * Stencils work by creating a new off-screen canvas on which to draw.
+ * The drawing functions called between createBegin and createEnd draw
+ * to that off-screen canvas, not the main off-screen canvas. When 
+ * createEnd is called, all following draw functions draw to the normal
+ * off-screen canvas. When select is called screen.useStencil is set to 
+ * true and the stencilNumber is remembered. The big change happens in
+ * flush. If a stencil is being used, flush behaves quite differently. 
+ * flush first draws the stencil to a third off-screen canvas, stencilCanvas,
+ * then draws the backCanvas to stencilCanvas with stencil mode enabled, 
+ * and then finally draws stencilCanvas to the on-screen canvas. This is 
+ * done so that only the final image is ever drawn to the screen, and it
+ * is drawn all at once. This makes sure that flush ensures discrete frames.
+ *  
+ */
+
+/**
+ * Starts the creation of a stencil with the given number.
+ * @param stencilNumber the number of the stencil about to be created.
+ */
+function jglStencilCreateBegin(stencilNumber) {
+	var canvas = document.createElement('canvas');
+	canvas.width = screen.width;
+	canvas.height = screen.height;
+	screen.stencils[stencilNumber] = canvas;
+	backCtx = canvas.getContext("2d");
+}
+
+/**
+ * Ends the creation of a stencil.
+ */
+function jglStencilCreateEnd() {
+	backCtx = backCanvas.getContext("2d");
+}
+
+/**
+ * Selects the stencil with the given number.
+ * @param stencilNumber the number of the stencil to select.
+ * @throw Number too large if the number given is greater than the number of stencils.
+ * @throw No stencil if the number does not correspond to a stencil.
+ */
+function jglStencilSelect(stencilNumber) {
+	if (stencilNumber >= screen.stencils.length) {
+		//Error
+		throw "StencilSelect: Number too large";
+	}
+	if (screen.stencils[stencilNumber] == null) {
+		// TODO: Not sure if javaScript works like that, need to check
+		throw "StencilSelect: No Stencil with that number";
+	}
+	
+	screen.useStencil = true;
+	screen.stencilSelected = stencilNumber;
+}
+
+/**
+ * Deselects the selected stencil, this will cause flush to act as normal.
+ */
+function jglStencilDeselect() {
+	screen.usStencil = false;
+}
+
+
 
